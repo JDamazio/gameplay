2249b950804f658817a69c5a1a54ade2b2d2b5f5
JDamazio <88113749+JDamazio@users.noreply.github.com>
2022-02-10 21:36:51 -0300

    Add "src"

---
+++ src/routes/auth.routes.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+
+import {Home} from '../screens/home';
+import {SignIn} from '../screens/signin';
+import { theme } from '../global/styles/theme';
+
+
+const { Navigator, Screen } = createStackNavigator();
+
+export function AuthRoutes() {
+    return(
+        <Navigator
+            headerMode="none"
+            screenOptions={{
+                cardStyle:{
+                    backgroundColor: theme.colors.background 
+                }
+                
+            }}
+        >
+            <Screen
+                name="SignIn"
+                component={SignIn}
+            />
+            <Screen
+                name="Home"
+                component={Home}
+            />
+        </Navigator>
+    )
+}
